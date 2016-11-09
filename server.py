dbbf0e45a6f5dd577dd76ca543b9ada4fcaf3ced
Austin Schwartz <schwar12@purdue.edu>
2016-11-09 15:34:58 -0500

    Who working

--- server.py
+++ server.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 
+import re
+import subprocess
+from json import dumps, loads, JSONEncoder, JSONDecoder
 from http.server import BaseHTTPRequestHandler,HTTPServer
 
 PORT = 57888
 
+def getWho():
+    # Split who on new lines
+    who = subprocess.check_output("who").decode().split('\n')
+
+    # Get first column
+    who = [line.split(' ')[0] for line in who]
+
+    # Remove empty strings
+    who = filter(None, who)
+
+    return list(set(who))
+
 class handler(BaseHTTPRequestHandler):
-    def get(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.wfile.write(b'Hello World!')
-        return
+    def do_GET(self):
+        if None != re.search('/who', self.path):
+            self.send_response(200)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+
+            self.wfile.write(dumps(
+                {'response': getWho()}).encode()
+            )
+        else:
+            self.send_response(403)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
 
 try:
     server = HTTPServer(('', PORT), handler)
